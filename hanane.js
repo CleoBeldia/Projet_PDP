f1bdf6696efd52d7580b0fe5606678557f27392c
Ayse1006 <57561442+Ayse1006@users.noreply.github.com>
2020-04-27 01:10:01 +0200

    Update hanane.js

--- hanane.js
+++ hanane.js
@@ -1,9 +1,11 @@
 let x = [];
 let y = [];
-console.log('x1 :',x);
-console.log('y1 :',y);
+let x1 = [];
+let y1 = [];
 
 
+
+//image
 let loadFile = function(event) {
     let image = document.getElementById('uploadImage');
     image.src = URL.createObjectURL(event.target.files[0]);
@@ -15,55 +17,108 @@ window.onload = function() {
         let file = f.files[0];
         fr = new FileReader();
         fr.onload = function() {
-            x = [];
-            y = [];
+            x1 = [];
+            y1 = [];
             let first =(fr.result).toString().split(' ');
             let second = first.toString().split('\n'); 
             let third = second.toString().split(','); 
             console.log('third : ', third);
             for (let i=1; i<(third.length)-1; i++){
                 if (i%2==0){
-                    y.push(parseInt(third[i]));
+                    y1.push(parseInt(third[i]));
                 } 
                 else {
-                    x.push(parseInt(third[i]));
+                    x1.push(parseInt(third[i]));
                 }
+                console.log(x1);
             }
         };
         fr.readAsText(file);
     }
-}
-
-function draw1(){
-    let ctx = document.getElementById('canvas').getContext('2d');
-    let img = new Image();
-
-    console.log('x',x);
-
-    img.onload = function() {
-        ctx.drawImage(img, 0, 0);
-        for (let i=0; i<=x.length; i++) {    
-            ctx.beginPath();
-            ctx.arc(x[i], 192-y[i], 2.5, 0, 5 * Math.PI);
-            ctx.fillStyle ='red';
-            ctx.fill();
+    let g = document.getElementById('inputTps1');
+    console.log(g);
+    g.onchange = function() {
+        let file2 = g.files[0];
+        gr = new FileReader();
+        gr.onload = function() {
+            x = [];
+            y = [];
+            let firstg =(gr.result).toString().split(' ');
+            let secondg = firstg.toString().split('\n'); 
+            let thirdg = secondg.toString().split(','); 
+            console.log('third : ', thirdg);
+            for (let i=1; i<(thirdg.length)-1; i++){
+                if (i%2==0){
+                    y.push(parseInt(thirdg[i]));
+                } 
+                else {
+                    x.push(parseInt(thirdg[i]));
+                }
+            }
         };
-        ctx.stroke();
-    };
-    let image = document.getElementById('inputImage');
-    img.src = URL.createObjectURL(image.files[0]);    
+        gr.readAsText(file2);
+    }
 }
 
+
+function isChecked() {
+    var checkBox = document.getElementById("buttonDraw");
+    var checkBox1 = document.getElementById("buttonDraw1");
+    var checkBox2 = document.getElementById("dis");
+
+    if (checkBox.checked == true){
+        draw(x,y,2.5,'red');
+    }if (checkBox.checked==false) {
+        draw(x,y,0,'red');
+    }
+    if (checkBox1.checked == true){
+        draw(x1,y1,2.5,'yellow');
+    }if (checkBox1.checked==false){
+        draw(x1,y1,0,'yellow');
+    }
+    if (checkBox2.checked==true){
+        distance(x,x1,y,y1);
+    }if (checkBox2.checked==false){
+        draw(x1,y1,0,'yellow');
+    }
+} 
+
+
+function draw(x,y,size,color){
+    let ctx = document.getElementById('canvas').getContext('2d');
+    for (let i=0; i<=x.length; i++) {    
+        ctx.beginPath();
+        ctx.arc(x[i], 192-y[i], size, 0, 2 * Math.PI);
+        ctx.fillStyle =color;
+        ctx.fill();
+        ctx.stroke();
+    }; 
+}
+
+
+//dist((x, y), (a, b)) = √(x - a)² + (y - b)²
+function distance(x,x1,y,y1){
+    let ctx = document.getElementById('canvas').getContext('2d');
+    for(let i=0; i<x.length; i++){
+        let moyx=-10+((x[i]+x1[i])/2);
+        let moyy=185 - ((y[i]+y1[i])/2);
+        let rep = Math.sqrt(Math.pow((x[i]-x1[i]),2) + Math.pow((y[i]-y1[i]),2));
+        rep = Math.round(rep * 100) / 100 ;
+        ctx.font = "10px Arial";
+        ctx.fillStyle = "aqua";
+        ctx.fillText(rep, moyx, moyy); 
+    }
+
+}
 
 
 function allowDrop(ev) {
     ev.preventDefault();
   }
-  
   function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
   }
-  
+
   function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
