00e437dbddb32da95c0916fe969bea05fcdc0220
Cleo <61013910+CleoBeldia@users.noreply.github.com>
2020-04-28 23:22:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let y = [];
 let x1 = [];
 let y1 = [];
 let dist=[];
+let sizepoint=3.5;
 
 //image
 let loadFile = function(event) {
@@ -65,21 +66,21 @@ function isChecked() {
     clearpoint();
     setTimeout(function(){
     if (a==true && b==false && c==false){
-        draw(x,y,2.5,'red');
+        draw(x,y,sizepoint,'red');
     }else if(a==true && b==true && c==false){
-        draw(x,y,2.5,'red');
-        draw(x1,y1,2.5,'yellow');
+        draw(x,y,sizepoint,'red');
+        draw(x1,y1,sizepoint,'yellow');
     }else if(a==true && b==false && c==true){
-        draw(x,y,2.5,'red');
+        draw(x,y,sizepoint,'red');
         distance(x,x1,y,y1);
     }else if(a== true && b==true && c==true){
-        draw(x,y,2.5,'red');
-        draw(x1,y1,2.5,'yellow');
+        draw(x,y,sizepoint,'red');
+        draw(x1,y1,sizepoint,'yellow');
         distance(x,x1,y,y1);
     }else if (a==false && b==true && c==false){
-        draw(x1,y1,2.5,'yellow');
+        draw(x1,y1,sizepoint,'yellow');
     }else if (a==false && b==true && c==true){
-        draw(x1,y1,2.5,'yellow');
+        draw(x1,y1,sizepoint,'yellow');
         distance(x,x1,y,y1);
     }else if (a==false && b==false && c==true){
         distance(x,x1,y,y1);
@@ -95,7 +96,7 @@ function clearpoint(){
     let input = document.getElementById('inputImage');
     image.src = URL.createObjectURL(input.files[0]);
     image.onload = function() {
-    ctx.drawImage(image,0,0);
+    ctx.drawImage(image,0,0,384,384);
     }
 }
 
@@ -103,7 +104,7 @@ function draw(x,y,size,color){
     let ctx = document.getElementById('canvas').getContext('2d');
     for (let i=0; i<=x.length; i++) {    
         ctx.beginPath();
-        ctx.arc(x[i], 192-y[i], size, 0, 2 * Math.PI);
+        ctx.arc(x[i]*2, 384-(y[i]*2), size, 0, 2 * Math.PI);
         ctx.fillStyle =color;
         ctx.fill();
         ctx.stroke();
@@ -115,19 +116,19 @@ function draw(x,y,size,color){
 function distance(x,x1,y,y1){
     let ctx = document.getElementById('canvas').getContext('2d');
     for(let i=0; i<x.length; i++){
-        let moyx=-10+((x[i]+x1[i])/2);
+        let moyx=((x[i]+x1[i])/2);
         let moyy=185 - ((y[i]+y1[i])/2);
         let rep = Math.sqrt(Math.pow((x[i]-x1[i]),2) + Math.pow((y[i]-y1[i]),2));
         rep = Math.round(rep * 100) / 100 ;
         dist.push(rep);
-        ctx.font = "10px Arial";
-        ctx.fillStyle = "black";
-        ctx.fillText(i, moyx, moyy); 
+        ctx.font = "15px Arial";
+        ctx.fillStyle = "springgreen";
+        ctx.fillText(dist[i], moyx*2, moyy*2); 
         console.log(dist);
     }
     displayDistance(dist);
 }
-
+/*
 function displayDistance(dist){
     for (let i=0; i<dist.length;i++){
         let afficheDistance = document.createElement("p");
@@ -138,7 +139,7 @@ function displayDistance(dist){
     }
 
 }
-
+*/
 function allowDrop(ev) {
     ev.preventDefault();
   }
@@ -155,7 +156,7 @@ function allowDrop(ev) {
     let input = document.getElementById('inputImage');
     image.src = URL.createObjectURL(input.files[0]);
     image.onload = function() {
-    ctx.drawImage(image,0,0);    
+    ctx.drawImage(image,0,0,384,384);    
     }
   }
 
@@ -163,5 +164,3 @@ download_beetle = function(image) {
     var enregister = canvas.toDataURL("image/jpeg");
     image.href = enregister;
   };
-
-//
