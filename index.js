4b40da81cbdc9b18c60118f2874acbcaf1600514
Ayse1006 <57561442+Ayse1006@users.noreply.github.com>
2020-04-28 16:03:59 +0200

    Update and rename hanane.js to index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ let x = [];
 let y = [];
 let x1 = [];
 let y1 = [];
-
-
+let dist=[];
 
 //image
 let loadFile = function(event) {
@@ -11,53 +10,48 @@ let loadFile = function(event) {
     image.src = URL.createObjectURL(event.target.files[0]);
 };
 
+function listpoint(third,x,y){
+    for (let i=1; i<(third.length)-1; i++){
+        if (i%2==0){
+            y.push(parseInt(third[i]));
+        } 
+        else {
+            x.push(parseInt(third[i]));
+        }
+    }
+}
 window.onload = function() {
     let f = document.getElementById('inputTps');
     f.onchange = function() {
         let file = f.files[0];
         fr = new FileReader();
         fr.onload = function() {
-            x1 = [];
-            y1 = [];
+            x = [];
+            y = [];
             let first =(fr.result).toString().split(' ');
             let second = first.toString().split('\n'); 
             let third = second.toString().split(','); 
-            console.log('third : ', third);
-            for (let i=1; i<(third.length)-1; i++){
-                if (i%2==0){
-                    y1.push(parseInt(third[i]));
-                } 
-                else {
-                    x1.push(parseInt(third[i]));
-                }
-                console.log(x1);
-            }
+            listpoint(third,x,y)
         };
         fr.readAsText(file);
     }
     let g = document.getElementById('inputTps1');
-    console.log(g);
     g.onchange = function() {
         let file2 = g.files[0];
         gr = new FileReader();
         gr.onload = function() {
-            x = [];
-            y = [];
+            x1 = [];
+            y1 = [];
             let firstg =(gr.result).toString().split(' ');
             let secondg = firstg.toString().split('\n'); 
             let thirdg = secondg.toString().split(','); 
-            console.log('third : ', thirdg);
-            for (let i=1; i<(thirdg.length)-1; i++){
-                if (i%2==0){
-                    y.push(parseInt(thirdg[i]));
-                } 
-                else {
-                    x.push(parseInt(thirdg[i]));
-                }
-            }
+            listpoint(thirdg,x1,y1)
         };
         gr.readAsText(file2);
+    
+        console.log(x,'\n',x1,'\n',y,'\n',y1);
     }
+    
 }
 
 
@@ -65,24 +59,45 @@ function isChecked() {
     var checkBox = document.getElementById("buttonDraw");
     var checkBox1 = document.getElementById("buttonDraw1");
     var checkBox2 = document.getElementById("dis");
-
-    if (checkBox.checked == true){
+    var a = checkBox.checked;
+    var b = checkBox1.checked;
+    var c = checkBox2.checked;
+    clearpoint();
+    setTimeout(function(){
+    if (a==true && b==false && c==false){
         draw(x,y,2.5,'red');
-    }if (checkBox.checked==false) {
-        draw(x,y,0,'red');
-    }
-    if (checkBox1.checked == true){
+    }else if(a==true && b==true && c==false){
+        draw(x,y,2.5,'red');
         draw(x1,y1,2.5,'yellow');
-    }if (checkBox1.checked==false){
-        draw(x1,y1,0,'yellow');
-    }
-    if (checkBox2.checked==true){
+    }else if(a==true && b==false && c==true){
+        draw(x,y,2.5,'red');
         distance(x,x1,y,y1);
-    }if (checkBox2.checked==false){
-        draw(x1,y1,0,'yellow');
+    }else if(a== true && b==true && c==true){
+        draw(x,y,2.5,'red');
+        draw(x1,y1,2.5,'yellow');
+        distance(x,x1,y,y1);
+    }else if (a==false && b==true && c==false){
+        draw(x1,y1,2.5,'yellow');
+    }else if (a==false && b==true && c==true){
+        draw(x1,y1,2.5,'yellow');
+        distance(x,x1,y,y1);
+    }else if (a==false && b==false && c==true){
+        distance(x,x1,y,y1);
+    }else{
+        clearpoint();
     }
-} 
+    }, 5);
+}
 
+function clearpoint(){
+    let ctx = document.getElementById('canvas').getContext('2d');
+    let image = new Image();
+    let input = document.getElementById('inputImage');
+    image.src = URL.createObjectURL(input.files[0]);
+    image.onload = function() {
+    ctx.drawImage(image,0,0);
+    }
+}
 
 function draw(x,y,size,color){
     let ctx = document.getElementById('canvas').getContext('2d');
@@ -104,13 +119,25 @@ function distance(x,x1,y,y1){
         let moyy=185 - ((y[i]+y1[i])/2);
         let rep = Math.sqrt(Math.pow((x[i]-x1[i]),2) + Math.pow((y[i]-y1[i]),2));
         rep = Math.round(rep * 100) / 100 ;
+        dist.push(rep);
         ctx.font = "10px Arial";
-        ctx.fillStyle = "aqua";
-        ctx.fillText(rep, moyx, moyy); 
+        ctx.fillStyle = "black";
+        ctx.fillText(i, moyx, moyy); 
+        console.log(dist);
+    }
+    displayDistance(dist);
+}
+
+function displayDistance(dist){
+    for (let i=0; i<dist.length;i++){
+        let afficheDistance = document.createElement("p");
+        afficheDistance.setAttribute("class","distance");
+        document.getElementById("div").appendChild(afficheDistance);
+        let result ="Point: "+ i + " : " +dist[i] ;
+        afficheDistance.innerHTML =result;
     }
 
 }
-
 
 function allowDrop(ev) {
     ev.preventDefault();
@@ -129,10 +156,12 @@ function allowDrop(ev) {
     image.src = URL.createObjectURL(input.files[0]);
     image.onload = function() {
     ctx.drawImage(image,0,0);    
-    }    
+    }
   }
 
 download_beetle = function(image) {
     var enregister = canvas.toDataURL("image/jpeg");
     image.href = enregister;
   };
+
+//
