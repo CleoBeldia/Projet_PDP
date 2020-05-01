a62b90c333cb0a2de0eba34f7c2277e332a0dfbe
Ayse1006 <57561442+Ayse1006@users.noreply.github.com>
2020-05-01 17:33:29 +0200

    Update statistic.js

--- js/statistic.js
+++ js/statistic.js
@@ -1,45 +1,65 @@
-function Checked(){
-    var p1 = document.getElementById("part1");
-    var p2 = document.getElementById("part2");
-    var p3 = document.getElementById("part3");
-    var p4 = document.getElementById("part4");
-    var p5 = document.getElementById("part5");
+var DATA = [];
+var LABELS = [];
 
-    var a = p1.checked;
-    var b = p2.checked;
-    var c = p3.checked;
-    var d = p4.checked;
-    var e = p5.checked;
-    var choice=0;
-   
-    //setTimeout(function(){
-        if (a==true && b==false && c==false && d==false && e==false){  
-            choice=1;
-        }  
-        if (a==false && b==true && c==false && d==false && e==false){  
-            choice=2;
-        }
-        if (a==false && b==false && c==true && d==false && e==false){ 
-            choice=3;
-        }
-        if (a==false && b==false && c==false && d==true && e==false){ 
-            choice=4;
-        }
-        if (a==false && b==false && c==false && d==false && e==true){ 
-            choice=5;
-        }
-
-    return choice;
+window.onload = function() {
+    let f = document.getElementById('fileUpload');
+    f.onchange = function() {
+        let file = f.files[0];
+        fr = new FileReader();
+        fr.onload = function(event) {
+            var csv = event.target.result;
+            DATA=[];
+            //split and get the rows in an array
+            var rows = csv.split('\n');
+            var cols = rows[0].split(',');
+            for (var j = 1; j < rows.length; j++) {
+                var value = rows[j].split(',');
+                DATA.push(value[6]);
+                LABELS.push(value[0]);
+            }
+        };
+        fr.readAsText(file);
+    }
 }
 
-function renderChart(data,labels) { 
-    var ctx = document.getElementById("myChart").getContext('2d');
+function Process() {
+    var fileInput = document.getElementById('fileUpload');
+    var file = fileInput.value.split(/(\\|\/)/g).pop();
+    console.log(file);
+    console.log(DATA,LABELS);
+    if (file == 'elytra.csv'){
+        for(var j = 0; j<11; j++){
+            graph(DATA, LABELS, 11,j);
+        }
+    } if (file == 'pronotum.csv'){
+        for(var k = 0; k<8; k++){
+            graph(DATA,LABELS, 8,k);
+        }
+    }if (file == 'md.csv'){ 
+        for(var l = 0; l<18; l++){
+            graph(DATA, LABELS, 18,l);
+        }
+        
+    }if (file == 'mg.csv'){ 
+        for(var m = 0; m<16; m++){
+            graph(DATA, LABELS, 16,m);
+        }
+       
+    }if (file== 'tete.csv'){ 
+        for(var n = 0; n<10; n++){
+            graph(DATA, LABELS, 10,n);
+        }
+    }
+}
+
+function renderChart(data,labels,landnb,canvas) { 
+    var ctx = document.getElementById(canvas).getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: labels,
             datasets: [{
-                label: 'L1',
+                label: landnb,
                 data: data,
             }]
         },
@@ -48,7 +68,7 @@ function renderChart(data,labels) {
                 position : 'bottom',
                 labels:{
                     fontSize: 12,
-                    boxWidth: 12,
+                    boxWidth: 20,
                     usePointStyle: true,
                 }
             }
@@ -57,72 +77,15 @@ function renderChart(data,labels) {
 }
 
 
-
-function processFile(){ 
-    var file = document.querySelector('#fileUpload').files[0];
-    var reader = new FileReader();
-    reader.readAsText(file);
-    //When the file finish load
-    reader.onload = function(event) {
-      //get the file.
-      var csv = event.target.result;
-      console.log('csv' + csv);
-      //split and get the rows in an array
-      var rows = csv.split('\n');
-      console.log(rows);
-      var cols = rows[0].split(',');
-      console.log(cols);
-
-        var DATA = [];
-        var LABELS = [];
-        for (var j = 1; j < rows.length; j++) {
-          /*the value of the current column.
-          Do whatever you want with the value*/
-          var value = rows[j].split(',');
-          console.log(value[6]);
-          DATA.push(value[6]);
-          LABELS.push(value[0]);
-        }
-        console.log(DATA,LABELS);
-        var data=[];
-        var labels=[]
-        var choice = Checked();
-        if (choice==1){    
-            for (var i = 0 ; i< DATA.length ; i=i+11){
-                data.push(DATA[i]);
-                labels.push(LABELS[i]);
-            }
-        renderChart(data,labels);
-        }
-        if (choice==2){    
-            for (var i = 1 ; i< DATA.length ; i=i+8){
-                data.push(DATA[i]);
-                labels.push(LABELS[i]);
-        }
-        renderChart(data,labels);
-        }
-        if (choice==3){    
-            for (var i = 1 ; i< DATA.length ; i=i+18){
-                data.push(DATA[i]);
-                labels.push(LABELS[i]);
-            }
-        renderChart(data,labels);
-        }
-        if (choice==4){   
-            for (var i = 1 ; i< DATA.length ; i=i+16){
-                data.push(DATA[i]);
-                labels.push(LABELS[i]);
-            }
-        renderChart(data,labels);
-        }
-        if (choice==5){  
-            for (var i = 1 ; i< DATA.length ; i=i+10){
-                data.push(DATA[i]);
-                labels.push(LABELS[i]);
-            }
-        renderChart(data,labels);
+function graph(DATA, LABELS,LandmarkNb,j){
+    var data=[];
+    var labels=[];
+        for (var i = 0+j ; i< DATA.length ; i=i+LandmarkNb){
+            data.push(DATA[i]);
+            labels.push(LABELS[i]);
+            renderChart(data,labels,('L'+(j+1)),('myChart'+(j+1)));
         }
         console.log(data,labels);
-    }
-   
-    }
+        
+}
+
