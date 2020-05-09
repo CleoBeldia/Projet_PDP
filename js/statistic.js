a6aef9d9cd0e0f218a14e68a9ea4d389873dde06
Ayse1006 <57561442+Ayse1006@users.noreply.github.com>
2020-05-10 01:14:02 +0200

    it works

--- js/statistic.js
+++ js/statistic.js
@@ -32,43 +32,48 @@ function Process() {
     console.log(file);
     if (file == 'elytra.csv') {
         let LM1 = [],LM2 = [],LM3 = [],LM4 = [],LM5 = [],LM6 = [],LM7 = [],LM8 = [],LM9 = [],LM10 = [],LM11 = [];
-        let L = new Array(293);
+        let L =[]
+        for (let j = 0 ; j<293;j++){
+            L.push(j);
+        }
         for(let i=0; i<LAND.length;i++){
             if (LAND[i]=='1') {
                 LM1.push(DATA[i]);
-                renderChart(LM1, L, 'L1');
             } else if (LAND[i] =='2') {
                 LM2.push(DATA[i]);
-                renderChart(LM2, L, 'L2');
             } else if (LAND[i] =='3') {
                 LM3.push(DATA[i]);
-                renderChart(LM3, L, 'L3');
             } else if (LAND[i] =='4') {
                 LM4.push(DATA[i]);
-                renderChart(LM4, L, 'L4');
             } else if (LAND[i] =='5') {
                 LM5.push(DATA[i]);
-                renderChart(LM5, L, 'L5');
             } else if (LAND[i] =='6') {
                 LM6.push(DATA[i]);
-                renderChart(LM6, L, 'L6');
             } else if (LAND[i] =='7') {
                 LM7.push(DATA[i]);
-                renderChart(LM7, L, 'L7');
-            } else if (LAND =='8') {
+            } else if (LAND[i] =='8') {
                 LM8.push(DATA[i]);
-                renderChart(LM8, L, 'L8');
             } else if (LAND[i] =='9') {
                 LM9.push(DATA[i]);
-                renderChart(LM9, L, 'L9');
             } else if (LAND[i] =='10') {
                 LM10.push(DATA[i]);
-                renderChart(LM10, L, 'L10');
             } else if (LAND[i] =='11') {
                 LM11.push(DATA[i]);
-                renderChart(LM11, L, 'L11');
             }
         }
+        renderChart(LM1, L, 'L1');
+        renderChart(LM2, L, 'L2');
+        renderChart(LM3, L, 'L3');
+        renderChart(LM4, L, 'L4');
+        renderChart(LM5, L, 'L5');
+        renderChart(LM6, L, 'L6');
+        renderChart(LM7, L, 'L7');
+        renderChart(LM8, L, 'L8');
+        renderChart(LM9, L, 'L9');
+        renderChart(LM10, L, 'L10');
+        renderChart(LM11, L, 'L11');
+
+
     } else {
         alert('pls upload the right file');
     }
