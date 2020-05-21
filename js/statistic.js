7581afc6b1710d91b1145bd8c158f53d3301843e
HassainiaHanane <61012015+HassainiaHanane@users.noreply.github.com>
2020-05-21 14:34:29 +0200

    Update statistic.js

--- js/statistic.js
+++ js/statistic.js
@@ -231,6 +231,9 @@ function renderChart(data, labels, name) {
                 data: data,//y
                 backgroundColor: 'rgba(0, 128, 128, 0.116)',
                 borderColor: "rgb(0, 128, 128)",
+                pointBackgroundColor : 'rgb(175, 88, 88)',
+                pointBorderColor : 'rgb(175, 88, 88)',
+                pointRadius : 2,
             }]
         },
         options: {
