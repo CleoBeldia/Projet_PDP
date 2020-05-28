ce3194a1831b94f3da6bcb7ce96b60478961eb52
HassainiaHanane <61012015+HassainiaHanane@users.noreply.github.com>
2020-05-28 05:08:46 +0200

    Update homePage.js

--- js/homePage.js
+++ js/homePage.js
@@ -1,1 +1,19 @@
  
+let open = function(){
+    document.getElementById('old_side').style.display = 'none';
+    document.getElementById('new_side').style.display = 'block';
+}
+
+let close = function(){
+    document.getElementById('old_side').style.display = 'block';
+    document.getElementById('new_side').style.display = 'none';
+}
+
+let prevent = function(){
+    let button_old = document.getElementById('side_bar');
+    let button_new = document.getElementById('close');
+    button_old.addEventListener("click", open);
+    button_new.addEventListener("click", close);
+}
+
+window.onload = prevent;
