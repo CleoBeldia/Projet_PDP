6fbf8c52d6d27ccaecdb3eb9cadf4c9402aa2f0e
HassainiaHanane <61012015+HassainiaHanane@users.noreply.github.com>
2020-05-29 06:54:28 +0200

    Update homePage.js

--- js/homePage.js
+++ js/homePage.js
@@ -1,14 +1,16 @@
 
+//Fonction permettant l'affichage de l'élément contenant les liens utiles et suppression de la visibilité du bouton 'LINKS'
 let open = function(){
     document.getElementById('old_side').style.display = 'none';
     document.getElementById('new_side').style.display = 'block';
 }
 
-let close = function(){
+//Fonction permettant l'affichage du bouton 'LINKS' et la suppression de la visibilité de l'élément contenant les liens utiles 
     document.getElementById('old_side').style.display = 'block';
     document.getElementById('new_side').style.display = 'none';
 }
 
+//Fonction permettant l'appel des deux précédentes fonctions lorsque l'utilisateur clique sur des boutons en particulier
 let prevent = function(){
     let button_old = document.getElementById('side_bar');
     let button_new = document.getElementById('close');
